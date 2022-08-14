786672ff076997571c0c0c33d876559dc9951781
Anaid Candido <anaid.candido@gmail.com>
2022-08-14 22:30:53 +0100

    added feeding and ignoring fuctions

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,14 @@ function App() {
             name: "Walk",
             handler: () => loadDataFromEndpoint("/digipet/walk"),
           },
-          { name: "Feed" },
+          {
+            name: "Feed",
+            handler: () => loadDataFromEndpoint("/digipet/feed"),
+          },
+          {
+            name: "Ignore",
+            handler: () => loadDataFromEndpoint("/digipet/ignore"),
+          }
         ]}
       />
     </main>
